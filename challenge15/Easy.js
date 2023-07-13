3ac6f476202f9ab0c0d219299968d38cdf270bae
Gulcan Yerlikaya <glcn.yerlikaya@gmail.com>
2023-07-13 16:08:38 +0300

    -

---
+++ challenge15/Easy.js
@@ -0,0 +1,7 @@
+console.log("hi from easy.js");
+
+const dogumyılı = prompt("Doğum yılınız");
+
+const yas = new Date().getFullYear()-dogumyılı;
+
+document.write("yaşınız:"+yas);
---
+++ challenge15/Medium.js
@@ -0,0 +1,8 @@
+console.log("hi from medium.js");
+
+const metin = prompt("metin giriniz");
+
+const harf = "a";
+const tekrarsayisi = 2;
+
+document.write("en çok tekrar eden harf :" + harf +"  "+ "ve sayısı:" + tekrarsayisi);
