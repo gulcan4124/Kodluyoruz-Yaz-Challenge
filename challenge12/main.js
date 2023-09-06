fb355194d536cdd7ab7d5f4e7f98aa929a602622
Gulcan Yerlikaya <glcn.yerlikaya@gmail.com>
2023-09-06 12:16:12 +0300

    challenge12

---
+++ challenge12/main.js
@@ -0,0 +1,29 @@
+console.log("hello");
+
+// easy start
+
+// Bir araba saatte 60 km hızla hareket ediyor. Bu araba kaç saatte 240 km yol alır?
+
+let araba = 60;
+let yol = 240;
+let zaman = yol/araba;
+
+console.log ("araba" + " " +zaman +" " + "saatte yol alır");
+
+// easy end
+
+// medium start
+
+// Bir çiftlikte toplamda 36 baş tavuk ve koyun bulunmaktadır.
+// Bu hayvanlardan toplamda 100 bacak çıkmaktadır. Çiftlikte kaçar tane tavuk ve koyun olduğunu bulan kod parçacığını yazar mısın?
+
+let hayvanToplam = 36;
+let bacakToplam = 100;
+
+let tavukSayısı = (4*hayvanToplam-bacakToplam)/2;
+
+let koyunSayısı = hayvanToplam-tavukSayısı;
+
+console.log("tavuk sayısı " + tavukSayısı);
+console.log ("koyun sayısı " + koyunSayısı);
+
