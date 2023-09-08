e06ee340b0961f0c61fab6f554caae84b8afa244
Gulcan Yerlikaya <glcn.yerlikaya@gmail.com>
2023-09-08 11:58:37 +0300

    challenge8

---
+++ challenge8/main.js
@@ -0,0 +1,17 @@
+console.log("slm");
+
+//easy
+
+// Kullanıcıdan aldığın sayının karesini hesaplayarak ekrana yazdıran kod parçacığını yazar mısın?
+
+let sayı = prompt("karesi alınacak sayıyı giriniz:");
+
+
+let karesi = sayı*sayı;
+
+    
+
+console.log(sayı + " nın karesi: " + karesi);
+
+// medium
+
