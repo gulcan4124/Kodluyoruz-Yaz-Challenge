f8545470784cccaf8ee1b98f6086eba01d7d776d
Gulcan Yerlikaya <glcn.yerlikaya@gmail.com>
2023-09-08 13:35:45 +0300

    challenge1

---
+++ challenge1/main.js
@@ -0,0 +1,18 @@
+console.log("sonunda son hafta");
+
+// medium
+
+let hedef = 36
+let aySayisi = 12
+
+let kitapSayisi = hedef / aySayisi;
+
+console.log("ayda " + kitapSayisi);
+
+// hard
+
+let yarışmacı1 =Number(15);
+let yarışmacı2 =Number(20);
+
+let cevap = yarışmacı2/(yarışmacı2-yarışmacı1);
+console.log("cevap " + cevap);
