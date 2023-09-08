01cb4e040b8811acb1ec7990c391af905a94f4f4
Gulcan Yerlikaya <glcn.yerlikaya@gmail.com>
2023-09-08 13:20:39 +0300

    challenge3

---
+++ challenge3/main.js
@@ -0,0 +1,14 @@
+console.log("helloooo")
+
+// medium
+
+// Bir mağazada bir kitap 80 TL, bir defter 20 TL ve bir kalem 5 TL. 
+//Bir müşteri 2 kitap, 3 defter ve 4 kalem aldı. Müşteri ne kadar ödeme yapmalı?
+
+let kitap = Number(80);
+let defter = Number(20);
+let kalem = Number(5);
+
+let toplam = 2*kitap + 3*defter + 4*kalem;
+
+console.log("alışveriş toplamı: " + toplam + " tl");
